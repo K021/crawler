ae6f7f1417e65026f43510a75d340c8eb685aa49
Kim Jooeon <joo2theeon@gmail.com>
2017-09-25 10:53:54 +0900

    add(main.py, html_body.html) complete basic crawler

--- crawler.py
+++ crawler.py
@@ -1,86 +1,125 @@
 import requests, os, pickle
-from utils import Webtoon, get_webtoon_episode_list, get_webtoon_list
+from utils import Webtoon, get_webtoon_episode_list, get_total_webtoon_list
 from utils import html_head, html_body, html_bottom
 
-class NaverWebToonCrawlar():
+class NaverWebtoonCrawlar():
     def __init__(self, webtoon_name=None):
+        # 한 웹툰을 골라 그 정보를 네임드 튜플로 저장 (이름, id, 썸네일 소스)
         self.webtoon = self._webtoon_id_init(webtoon_name)
+        # 저장된 episode_list 있으면 불러옴
         self.episode_list = list()
         self.load(init=True)
-        print(self.webtoon)
 
+    # 문자열로 출력시 웹툰 정보 제공
+    def __str__(self):
+        return self.webtoon
+
+    # NaverWebtoonCrawlar 클래스 하나가 한 웹툰 정보를 가지도록 초기화하는 내부함수
     def _webtoon_id_init(self,webtoon_name=None):
+        # 웹툰 이름을 인자로 주지 않았을 경우, 입력 요구
         if not webtoon_name:
             webtoon_name = input('Type any name of webtoon: ')
+        # 입력된 문자열과 일치하는 웹툰을 찾아서 리스트로 저장. 없을 경우 빈 리스트.
         webtoon = self.find_webtoon(webtoon_name)
+        # 일치하는 웹툰이 없거나, 둘 이상일 때 무한 반복
         while len(webtoon) != 1:
+            # 없으면 재입력
             if not webtoon:
                 print(f"There's no webtoon named '{webtoon_name}'. Try again: ")
                 webtoon_name = input('Type any name of webtoon: ')
                 webtoon = self.find_webtoon(webtoon_name)
             print('==================================')
+            # 여러 개면 보여주고 하나 고르라고 하자
             for wt in webtoon:
                 print(wt.name)
             webtoon_name = input('You can choose one of these: ')
             webtoon = self.find_webtoon(webtoon_name)
+        # 리스트가 아닌 네임드 튜플(Webtoon)로 반환
         webtoon = webtoon[0]
         return webtoon
 
     @property
+    # 최신 에피소드까지 몇화인가? (휴재 및 프롤로그 포함)
     def total_episode_count(self):
+        # 첫 페이지의 episode_list 받아옴
         el = get_webtoon_episode_list(self.webtoon.title_id)
+        # 그 마지막 에피소드의 no 값 출력
         return int(el[0].no)
 
     @property
-    def up_to_date(self):
+    # 내 episode_list에 최신화가 있는가? ( True / False )
+    def is_up_to_date(self):
         return len(self.episode_list) == self.total_episode_count
+        # ------------------------------------------------
         # cur_episode_count = len(self.episode_list)
         # cur_episode_count = int(self.episode_list[0].no if self.episode_list != [] else 0)
         # total_episode_count = self.total_episode_count
         # return cur_episode_count == total_episode_count
+        # ------------------------------------------------
 
+    # 내 episode_list 업데이트
     def update_episode_list(self, force_update=False):
+        # force_update인 경우 기존 episode_list 비우기
         if force_update:
             self.episode_list = list()
+        # 아닌 경우 최신화가 있으면 업데이트 중단
+        elif self.is_up_to_date:
+            return
+        # 최신 episode를 담을 그릇
         new_episode_list = []
+        # 내가 가진 최신 episode 넘버
         recent_episode_no = int(self.episode_list[0].no if self.episode_list != [] else 0)
+        # 그 값과 최신화의 차이를 10으로 나눈 몫 + 1
+        # 에피소드를 가져오려면, 해당 웹툰 목록 페이지에서 긁어와야 하는데, 네이버 웹툰 페이지가 10화씩 보여주기 때문
         delta_no = (self.total_episode_count - recent_episode_no)//10 +1
+        # 각 페이지에서 에피소드 긁어옴
         for i in range(1,delta_no+1):
+            # 마지막 페이지에서 '내가 가진 최신화' 바로 전까지만 가져옴
             new_episode_list.extend(get_webtoon_episode_list(self.webtoon.title_id,i,recent_episode_no+1))
+        # 새 episode_list를 기존 episode_list 앞에 저장
         self.episode_list = new_episode_list + self.episode_list
-        for i in self.episode_list:
-            print(i)
+        # ------------------------------------------------
+        # for i in self.episode_list:
+        #     print(i)
+        # ------------------------------------------------
+        # episode_list를 pickle로 저장저장
         self.save()
+        # 딱히 리턴할 게 없으니 몇개나 업데이트 했는지 알려주자
         return len(new_episode_list)
 
     @staticmethod
+    # 문자열을 입력하면, 해당 문자열을 가진 웹툰 리스트를 반환
     def find_webtoon(user_input):
-        webtoon_list = get_webtoon_list()
+        webtoon_list = get_total_webtoon_list()
         user_webtoon = [ webtoon for webtoon in webtoon_list if user_input in webtoon.name ]
         return user_webtoon
 
-    def find_webtoon_search(user_input):
-        pass
-
+    # episode_list pickle로 저장
     def save(self, path=None):
+        # episode_list 디렉토리가 없으면 만들고
         if not os.path.isdir('./episode_list'):
             os.mkdir('./episode_list')
         path = './episode_list/{}.txt'.format(self.webtoon.title_id)
+        # pickle로 저장
         pickle.dump(self.episode_list,open(path,'wb'))  # 참조되는 곳이 없는 파일 객체는 닫힌다.
 
+    # episode_list pickle 불러오기
     def load(self, path=None, init=False):
         try:
             path = './episode_list/{}.txt'.format(self.webtoon.title_id)
             self.episode_list = pickle.load(open(path,'rb'))
+        # 파일이 없을 때 에러 처리
         except FileNotFoundError:
             if not init:
                 print('There no file on the path:{}'.format(path))
         return self.episode_list
 
-    def save_list_thumbnail(self):
-        os.makedirs(f'./webtoon/{self.webtoon.title_id}_thumbnail', exist_ok=True)
+    # 해당 webtoon의 전체 episode thumbnail을 저장
+    def save_all_thumbnail_in_episode_list(self):
+        # webtoon_id 폴더 안에 webtoon_id_thumbnail 폴더 생성. 이미 있어도 괜춘
+        os.makedirs(f'./webtoon/{self.webtoon.title_id}/{self.webtoon.title_id}_thumbnail', exist_ok=True)
         for ep in self.episode_list:
-            response = requests.get(ep.img_url)
+            response = requests.get(ep.thumbnail_url)
             path = f'./webtoon/{self.webtoon.title_id}/{self.webtoon.title_id}_thumbnail/{ep.no}.jpg'
             if not os.path.exists(path):
                 with open(path,'wb') as f:
@@ -98,9 +137,6 @@ class NaverWebToonCrawlar():
             f.write(html_bottom)
         return path
 
-    def get_episode_detail(self, episode):
-        pass
-
 
 
 # html_body = html_body.format(
@@ -112,7 +148,7 @@ class NaverWebToonCrawlar():
 
 # 선천적 얼간이들 http://comic.naver.com/webtoon/list.nhn?title_id=697680&weekday=mon
 
-nwc = NaverWebToonCrawlar()
+# nwc = NaverWebtoonCrawlar()
 
 
 # l = nwc.episode_list
--- utils.py
+++ utils.py
@@ -7,13 +7,13 @@ from episode import Episode
 
 
 # NamedTuple(Episode) 을 사용해서
-#   이미지 주소(img_url)
+#   썸네일 이미지 주소(thumbnail_url)
 #   에피소드 제목(title)
 #   에피소드 별점(rating)
 #   에피소드 등록일(created_date)
 # 를 가지는 NamedTuple의 리스트를 생성
 
-# Episode = namedtuple('Episode', ['no','title_id', 'img_url', 'title', 'rating', 'created_date'])
+# Episode = namedtuple('Episode', ['no','title_id', 'thumbnail_url', 'title', 'rating', 'created_date'])
 Webtoon = namedtuple('Webtoon', ['name','title_id','thumbnail_src'])
 
 html_head = '''<!DOCTYPE html>
@@ -37,9 +37,10 @@ html_bottom = '''  </table>
 </body>
 </html>
 '''
-def get_webtoon_list():
+
+def get_total_webtoon_list():
     """
-    네이버 웹툰의 모든 정보를 가져온다.
+    네이버에 등재된 모든 웹툰 정보 리스트를 가져온다.
     """
     response = requests.get('http://comic.naver.com/webtoon/weekday.nhn')
     soup = BeautifulSoup(response.text,'lxml')
@@ -68,12 +69,11 @@ def get_webtoon_list():
     return webtoon_list
 
 def get_webtoon_episode_list(title_id,page=1,min_no=1):
-    payload = {'title_id':title_id, 'page':page }
+    payload = {'titleId':title_id, 'page':page }
     response = requests.get('http://comic.naver.com/webtoon/list.nhn', params=payload)
     source = response.text
     soup = BeautifulSoup(source, 'lxml')
-
-    # 필요한 데이터들 (img_url, title, rating, created_date)추출
+    # 필요한 데이터들 (thumbnail_url, title, rating, created_date)추출
     episode_list = list()
     webtoon_table = soup.select_one('table.viewList')   # class = 'viewList'인 '첫번째' <table> 태그 안의 html 반환
     tr_list = webtoon_table.find_all('tr', recursive=False) # 그 안에서 모든 <tr> 태그의 html 반환. recursive=False 설정은 자식 태그만 반환
@@ -83,7 +83,7 @@ def get_webtoon_episode_list(title_id,page=1,min_no=1):
         if len(td_list) < 4:        # 배너광고 차단
             continue
         td_thumbnail = td_list[0]   # td_thumbnail에 해당하는 Tag의 첫 번째 a tag의 첫 번째 img태그의 'src'속성값
-        img_url = td_thumbnail.a.img.get('src')
+        thumbnail_url = td_thumbnail.a.img.get('src')
         title_url = td_thumbnail.a.get('href')
         parsed_url = urlparse(title_url)    # url 쪼개기 (6개 항목의 namedtuple 반환. 그러나 그 외의 다른 속성도 참조할 수 있음. 예를 들어 port, geturl(url전체 반환))
         queryset = parse_qs(parsed_url.query)   # query 쪼개기 (string:[string]구조의 딕셔너리로 반환)
@@ -104,7 +104,7 @@ def get_webtoon_episode_list(title_id,page=1,min_no=1):
         episode = Episode(     # Episode형 namedtuple객체 생성, episode_list에 추가
             no=no[0],
             title_id=title_id[0],
-            img_url=img_url,
+            thumbnail_url=thumbnail_url,
             title=title,
             rating=rating,
             created_date=created_date
